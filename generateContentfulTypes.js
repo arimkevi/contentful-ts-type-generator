e042904fe748e6a3508d74293094879dfd78f56e
Artur Rimkevitch <arimkevitch@reaktor.com>
2019-02-01 20:29:07 +0200

    refactor callbackhell to rxfengshui

--- generateContentfulTypes.js
+++ generateContentfulTypes.js
@@ -5,16 +5,17 @@ const program = require('commander');
 const contentful = require('contentful-management')
 const fs = require('fs')
 const path = require('path')
+const Rx = require('rx')
 
 const toInterfaceName = (s, prefix = '') => {
-
   s.replace(/-[[:alnum:]]/gm, (match) => { console.log('Match', match); return match.slice(1).toUpperCase()})
-
   return prefix + s.charAt(0).toUpperCase() + s.slice(1)
     .replace(/-[A-Za-z0-9_]/g, (match) => match.slice(1).toUpperCase())
 }
 
-function formatType(field, prefix = '', isArray = false) {
+const relativePath = path.normalize(path.relative(process.cwd(), __dirname))
+
+const formatType = (field, prefix = '', isArray = false) => {
   var type = field.type
   if (type === 'Text' || type === 'Symbol') {
     if(field.validations && field.validations[0] && field.validations[0].in) {
@@ -47,61 +48,66 @@ function formatType(field, prefix = '', isArray = false) {
   }
 }
 
-const generateContentfulTypes = (contentfulManagementClient, space, environment, outputFilePath = './contentfulTypes.d.ts', prefix = '', ignoredFields = [] ) => {
-  contentfulManagementClient.getSpace(space)
-    .then((space) => space.getEnvironment(environment))
-      .then((environment) => {
-        environment.getContentTypes({limit:1000, order: 'sys.id'})
-          .then(result => {
-            const items = result.items
-            var stream = fs.createWriteStream(outputFilePath)
-            stream.once('open', () => {
-              stream.write(`import { Entry, Asset } from 'contentful' \n`)
-              items.forEach(item => {
-                  stream.write(`export const ${toInterfaceName(item.sys.id, prefix)} = '${item.sys.id}'\n`)
-                  stream.write(`export interface ${toInterfaceName(item.sys.id, prefix)} { \n`)
-                  stream.write(`  //${item.name}\n`)
-                  stream.write(`  /* ${item.description} */\n`)
-                  item.fields.forEach(field => {
-                    if(field.omitted !== true && !ignoredFields.includes(field.id)) {
-                      var type = formatType(field, prefix)
-                      var nullable = field.required === true ? '' : '?'
-                      stream.write(`  ${field.id}${nullable}: ${type}  \n`)
-                    }
-                  })
-                  stream.write(`}\n\n`)
-              })
-
-              stream.end()
-              console.log('Generated ', path.normalize(outputFilePath))
-            })
-          })
-          .catch(e => {
-            console.error('error getting content types.', e)
-            process.exit(1);
-          })
-        })
-        .catch(e => {
-          console.error('error getting Contentful space environment.', e)
-          process.exit(1);
-        })
-    .catch(e => {
-      console.error('error getting Contentful space.', e)
-      process.exit(1);
-    })
+const getSpace = (client, space) => {
+  return Rx.Observable.fromPromise(client.getSpace(space))
 }
 
-const createClientAndGenerate = (space, accessToken, outputFilePath = './contentfulTypes.d.ts', host = 'cdn.contentful.com', environment = 'master', prefix = '', ignoredFields) => {
-  const client = contentful.createClient({
+const getEnvironment = (space, environment) => {
+  return Rx.Observable.fromPromise(space.getEnvironment(environment))
+}
+
+const getTypes = (environment) => {
+  return Rx.Observable.fromPromise(environment.getContentTypes({limit:1000, order: 'sys.id'}))
+}
+
+const createClient = (host, accessToken) => {
+  return contentful.createClient({
     host,
     accessToken,
     resolveLinks: true,
   })
-  generateContentfulTypes(client, space, environment, outputFilePath, prefix, ignoredFields.split(','))
 }
 
+const writeTypesToFile = (types, outputFilePath, prefix, ignoredFields = [] ) => {
+  const items = types.items
+  var stream = fs.createWriteStream(outputFilePath)
+  stream.once('open', () => {
+    stream.write(`import { Entry, Asset } from 'contentful' \n`)
+    items.forEach(item => {
+        stream.write(`export const ${toInterfaceName(item.sys.id, prefix)} = '${item.sys.id}'\n`)
+        stream.write(`export interface ${toInterfaceName(item.sys.id, prefix)} { \n`)
+        stream.write(`  //${item.name}\n`)
+        stream.write(`  /* ${item.description} */\n`)
+        item.fields.forEach(field => {
+          if(field.omitted !== true && !ignoredFields.includes(field.id)) {
+            var type = formatType(field, prefix)
+            var nullable = field.required === true ? '' : '?'
+            stream.write(`  ${field.id}${nullable}: ${type}  \n`)
+          }
+        })
+        stream.write(`}\n\n`)
+    })
+    stream.end()
+    console.log('Generated ', path.normalize(outputFilePath))
+  })
+}
+          
+const generateContentfulTypes = (space, accessToken, outputFilePath = './contentfulTypes.d.ts', host = 'cdn.contentful.com', environment = 'master', prefix = '', ignoredFields) => {
+  const client = createClient(host, accessToken)
+  getSpace(client, space)
+    .flatMap(space => getEnvironment(space, environment))
+    .flatMap(getTypes)
+    .subscribe({
+      onNext: (types) => {
+        writeTypesToFile(types, outputFilePath, prefix, ignoredFields.split(','))
+      },
+      onError: (e) => {
+        console.log(e)
+      },
+      onCompleted: () => {}
+    }) 
+}
 
-const relativePath = path.normalize(path.relative(process.cwd(), __dirname))
 program
   .arguments('<spaceId> <accessToken>')
   .usage(`[options] <spaceId> <accessToken>
@@ -112,7 +118,7 @@ program
   .option('-p, --prefix <value>', 'Name prefix for generated interfaces', '')
   .option('-h, --host [value]', 'Contentful host', 'api.contentful.com')
   .option('-i, --ignore [value]', 'Ignored field(s): a single field id or comma separated list of field ids', '')
-  .action((spaceId, accessToken, options) => createClientAndGenerate(spaceId, accessToken, options.output, options.host, options.environment, options.prefix, options.ignore))
+  .action((spaceId, accessToken, options) => generateContentfulTypes(spaceId, accessToken, options.output, options.host, options.environment, options.prefix, options.ignore))
   .parse(process.argv);
 
 if (!program.args.length) {
